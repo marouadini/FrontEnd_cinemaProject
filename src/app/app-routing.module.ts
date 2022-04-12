de88569a1fc84783b4c8589ff52fbe36b0df9192
zaa-inab <chakroun.zainab.ismo2020@gmail.com>
2022-04-12 21:34:21 +0000

    complete template integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { EvenementComponent } from './components/evenement/evenement.component';
+import { FilmsComponent } from './components/films/films.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:HomeComponent},
+{path:'films',component:FilmsComponent},
+{path:'contact',component:ContactComponent},
+{path:'evenement',component:EvenementComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
